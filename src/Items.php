1eafbf3cf597f8ae17fbc95e607f109e620cb75c
Jasper Koers <jasper@coding.agency>
2025-05-28 12:05:35 +0200

    ♻️ refactor(Items): simplify Items class properties and methods
    - change component property to a public string instead of a constant
    - remove unnecessary methods to streamline the class
    - update constructor to use nullable types for attribute and resolveCallback

--- src/Items.php
+++ src/Items.php
@@ -6,67 +6,12 @@ namespace Jaspur\Nova5ItemsField;
 
 use Laravel\Nova\Fields\Field;
 
-/**
- * @phpstan-type Item non-empty-string
- * @phpstan-type Items list<Item>
- */
 final class Items extends Field
 {
-    public const COMPONENT = 'nova5-items-field';
+    public string $component = 'nova5-items-field';
 
-    public function __construct(string $name, string|null $attribute = null, callable|null $resolveCallback = null)
+    public function __construct(string $name, ?string $attribute = null, $resolveCallback = null)
     {
         parent::__construct($name, $attribute, $resolveCallback);
-        $this->component = self::COMPONENT;
-    }
-
-
-    public function reorderable(bool $enabled = true): static
-    {
-        return $this->withMeta([
-            'reorderable' => $enabled,
-        ]);
-    }
-
-    public function split(string $delimiter): static
-    {
-        return $this->withMeta([
-            'split' => $delimiter,
-        ]);
-    }
-
-    public function draggable(bool $enabled = true): static
-    {
-        return $this->withMeta([
-            'draggable' => $enabled,
-        ]);
-    }
-
-    public function fullWidth(bool $enabled = true): static
-    {
-        return $this->withMeta([
-            'fullWidth' => $enabled,
-        ]);
-    }
-
-    /**
-     * @param non-empty-string $text
-     */
-    public function createButtonValue(string $text): static
-    {
-        return $this->withMeta([
-            'createButtonValue' => $text,
-        ]);
-    }
-    /**
-     * Beperk het aantal invoerbare items.
-     *
-     * @param int<1, max> $max
-     */
-    public function max(int $max): static
-    {
-        return $this->withMeta([
-            'max' => $max,
-        ]);
     }
 }
