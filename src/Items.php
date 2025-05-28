0eb10d32e8f5efbbcaa3abebb28e12951de94988
Jasper Koers <jasper@coding.agency>
2025-05-28 12:07:23 +0200

    ♻️ refactor(Items): change component property to public
    - update component property visibility from string to public
    - improve accessibility for component in the Items class

--- src/Items.php
+++ src/Items.php
@@ -8,7 +8,8 @@ use Laravel\Nova\Fields\Field;
 
 final class Items extends Field
 {
-    public string $component = 'nova5-items-field';
+    /** @var string */
+    public $component = 'nova5-items-field';
 
     public function __construct(string $name, ?string $attribute = null, $resolveCallback = null)
     {
